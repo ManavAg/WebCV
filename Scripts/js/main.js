010c1d7c4b9b31a21b4fc1e183d4a041292a4064
ManavAg <Manav.agarwal4@gmail.com>
2014-07-31 14:01:20 +0200

    removed obvious errors

--- Scripts/js/main.js
+++ Scripts/js/main.js
@@ -2,31 +2,31 @@ var Modal = React.createClass({
   render: function() {
     return (
         <div 
-          class="modal fade" 
+          className="modal fade" 
           id="myModal" 
           tabindex="-1" 
           role="dialog" 
           aria-labelledby="myModalLabel" 
           aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
                 <h4 
-                  class="modal-title heading" 
+                  className="modal-title heading" 
                   id="myModalLabel" 
                   style="display:inline-block; width:50%">
                   Modal title
                 </h4>
                 <button 
                   type="button " 
-                  class="btn btn-danger btn-xs" 
+                  className="btn btn-danger btn-xs" 
                   data-dismiss="modal" 
                   aria-hidden="true" 
                   style="margin-left:45%; width:3%; padding-left:0.5%; display:inline-block; position:fixed">
                   &times;
                 </button>
               </div>
-              <div class="modal-body text"></div>
+              <div className="modal-body text"></div>
             </div>
           </div>
 		    </div>
@@ -36,8 +36,10 @@ var Modal = React.createClass({
 var Apps = React.createClass({
   render: function() {
     return (
-        <div id="apps1"></div>
-        <div id="apps2"></div>
+        <div>
+          <div id="apps1"></div>
+          <div id="apps2"></div>
+        </div>
       );
   }
 });
@@ -45,12 +47,24 @@ var Contacts = React.createClass({
   render: function() {
     return (
       <div id="Contacts">
-        <a href="mailto:Manav.agarwal4@gmail.com"><img class="contact" src="Pics/Contacts/email.png"></a>
-          <a href="http://www.facebook.com/manava1"><img class="contact" src="Pics/Contacts/facebook.png"></a>
-          <a href="http://www.twitter.com/manava"><img class="contact" src="Pics/Contacts/twitter.png"></a>
-          <a href="http://www.github.com/manava"><img class="contact" src="Pics/Contacts/github.png"></a>
-          <a href="http://www.linkedin.com/manava"><img class="contact" src="Pics/Contacts/linkedin.png"></a>
-          <a href="http://www.youtube.com/manava"><img class="contact" src="Pics/Contacts/youtube.png"></a>
+        <a href="mailto:Manav.agarwal4@gmail.com">
+          <img className="contact" src="Pics/Contacts/email.png"/>
+        </a>
+        <a href="http://www.facebook.com/manava1">
+          <img className="contact" src="Pics/Contacts/facebook.png"/>
+        </a>
+        <a href="http://www.twitter.com/manava">
+          <img className="contact" src="Pics/Contacts/twitter.png"/>
+        </a>
+        <a href="http://www.github.com/manava">
+          <img className="contact" src="Pics/Contacts/github.png"/>
+        </a>
+        <a href="http://www.linkedin.com/manava">
+          <img className="contact" src="Pics/Contacts/linkedin.png"/>
+        </a>
+        <a href="http://www.youtube.com/manava">
+          <img className="contact" src="Pics/Contacts/youtube.png"/>
+        </a>
       </div>
     );
   }
@@ -58,19 +72,33 @@ var Contacts = React.createClass({
 var Signature = React.createClass({
   render: function() {
     return (
-      <img id="Signature" src="Pics/Signature.png" onClick="DownNameTab();"/>
+      <img 
+        id="Signature" 
+        src="Pics/Signature.png" 
+        onClick="DownNameTab();"/>
     );
   }
 });
 var Language = React.createClass({
   render: function() {
     return (
-      <div id="Lang1" class="ChangeLanguage" onClick="DownLangTab();">EN &#9660;</div>
-      <div id="Lang2" class="ChangeLanguage" onClick="SelectLang();"></div>
+      <div>
+        <div 
+          id="Lang1" 
+          className="ChangeLanguage" 
+          onClick="DownLangTab();">
+          EN &#9660;
+        </div>
+        <div 
+          id="Lang2" 
+          className="ChangeLanguage" 
+          onClick="SelectLang();">
+        </div>
+      </div>
     );
   }
 });
-var screen = React.createClass({
+var Screen = React.createClass({
   render: function() {
     return (
       <div id="screen">
@@ -82,4 +110,11 @@ var screen = React.createClass({
       </div>
     );
   }
+});
+
+$(document).ready(function(){
+    /* jshint ignore:start */
+  var $tabletBlock = document.getElementById('tablet');
+    React.renderComponent(<Screen />, $tabletBlock);
+    /* jshint ignore:end */
 });
